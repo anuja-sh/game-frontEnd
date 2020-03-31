4b658facde64066d879549029851cef319a1541b
Anuja <anuja.shingne@tcs.com>
2020-03-31 12:31:23 +0200

    updated url of server

--- js/app/main.js
+++ js/app/main.js
@@ -3,7 +3,7 @@
  * the game
  */
 $(document).ready(() => {
-	const url = "http://localhost:3000";
+	const url = "http://ec2-3-125-116-112.eu-central-1.compute.amazonaws.com:3000";
 
 	$("#start").click((event) => {
 		event.preventDefault();
